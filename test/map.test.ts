a2b1ef7826c516ff10bdf11ad7aeec478aa9c136
Alba Mendez <me@alba.sh>
2020-08-16 13:17:48 +0200

    ⚙️ test ConvMap instead of RawMap
    makes test much easier to read... testing both
    separately would probably be better, but I think
    it's no big deal

--- test/map.test.ts
+++ test/map.test.ts
@@ -1,11 +1,8 @@
-import { createMap, MapType, RawMap } from '../lib'
-import { asBuffer, asUint32Array } from '../lib/util'
+import { createMap, MapType, ConvMap, u32type } from '../lib'
 
-const u32 = (x: number) => asBuffer(Uint32Array.of(x))
-const u32get = (x: Uint8Array) => asUint32Array(x)[0]
-const sortKeys = (x: Iterable<[Buffer, Buffer]>) => [...x].sort((a, b) => u32get(a[0]) - u32get(b[0]))
+const sortKeys = (x: Iterable<[number, number]>) => [...x].sort((a, b) => a[0] - b[0])
 
-describe('RawMap tests', () => {
+describe('ConvMap tests', () => {
 
     it('map creation', () => {
         expect(() => createMap({
@@ -22,13 +19,13 @@ describe('RawMap tests', () => {
             valueSize: 4,
             maxEntries: 5,
         })
-        const map = new RawMap(ref)
+        const map = new ConvMap(ref, u32type, u32type)
 
         expect(map.ref).toBe(ref)
         ref.fd // should not throw
         ref.close() // should not throw
         expect(() => ref.fd).toThrowError('FD was closed')
-        expect(() => map.get(u32(0))).toThrowError('FD was closed')
+        expect(() => map.get(0)).toThrowError('FD was closed')
         ref.close() // should not throw
     })
 
@@ -39,39 +36,39 @@ describe('RawMap tests', () => {
             valueSize: 4,
             maxEntries: 5,
         })
-        const map = new RawMap(ref)
+        const map = new ConvMap(ref, u32type, u32type)
 
         // ENOTSUP on some kernels, EINVAL on others
-        expect(() => map.getDelete(u32(2))).toThrow()
+        expect(() => map.getDelete(2)).toThrow()
         
         expect(sortKeys(map)).toStrictEqual([])
-        expect(map.get( u32(2) )).toBeUndefined()
-        //expect(map.getDelete( u32(2) )).toBeUndefined()
-        expect(map.delete( u32(2) )).toBe(false)
+        expect(map.get(2)).toBeUndefined()
+        //expect(map.getDelete(2)).toBeUndefined()
+        expect(map.delete(2)).toBe(false)
         
-        map.set( u32(2), u32(5) )
-        expect(sortKeys(map)).toStrictEqual([ [u32(2), u32(5)] ])
-        expect(map.get( u32(2) )).toStrictEqual( u32(5) )
+        map.set(2, 5)
+        expect(sortKeys(map)).toStrictEqual([ [2, 5] ])
+        expect(map.get(2)).toStrictEqual(5)
         
-        map.set( u32(0), u32(4) )
-        expect(sortKeys(map)).toStrictEqual([ [u32(0), u32(4)], [u32(2), u32(5)] ])
-        expect(map.get( u32(2) )).toStrictEqual( u32(5) )
-        expect(map.get( u32(0) )).toStrictEqual( u32(4) )
+        map.set(0, 4)
+        expect(sortKeys(map)).toStrictEqual([ [0, 4], [2, 5] ])
+        expect(map.get(2)).toStrictEqual(5)
+        expect(map.get(0)).toStrictEqual(4)
         
-        map.set( u32(2), u32(7) )
-        expect(sortKeys(map)).toStrictEqual([ [u32(0), u32(4)], [u32(2), u32(7)] ])
-        expect(map.get( u32(2) )).toStrictEqual( u32(7) )
-        expect(map.get( u32(0) )).toStrictEqual( u32(4) )
+        map.set(2, 7)
+        expect(sortKeys(map)).toStrictEqual([ [0, 4], [2, 7] ])
+        expect(map.get(2)).toStrictEqual(7)
+        expect(map.get(0)).toStrictEqual(4)
         
-        expect(map.delete( u32(2) )).toBe(true)
-        expect(sortKeys(map)).toStrictEqual([ [u32(0), u32(4)] ])
-        expect(map.get( u32(2) )).toBeUndefined()
-        expect(map.get( u32(0) )).toStrictEqual( u32(4) )
+        expect(map.delete(2)).toBe(true)
+        expect(sortKeys(map)).toStrictEqual([ [0, 4] ])
+        expect(map.get(2)).toBeUndefined()
+        expect(map.get(0)).toStrictEqual(4)
 
-        //expect(map.getDelete( u32(0) )).toStrictEqual( u32(4) )
+        //expect(map.getDelete(0)).toStrictEqual(4)
         //expect(sortKeys(map)).toStrictEqual([])
-        //expect(map.get( u32(0) )).toBeUndefined()
-        //expect(map.delete( u32(0) )).toBe(false)
+        //expect(map.get(0)).toBeUndefined()
+        //expect(map.delete(0)).toBe(false)
 
         map.ref.close()
     })
@@ -83,18 +80,18 @@ describe('RawMap tests', () => {
             valueSize: 4,
             maxEntries: 5,
         })
-        const map = new RawMap(ref)
+        const map = new ConvMap(ref, u32type, u32type)
 
-        map.set( u32(0), u32(4) )
-        map.set( u32(2), u32(8) )
-        map.set( u32(3), u32(7) )
+        map.set(0, 4)
+        map.set(2, 8)
+        map.set(3, 7)
         
-        expect(sortKeys(map)).toStrictEqual([ [u32(0), u32(4)], [u32(2), u32(8)], [u32(3), u32(7)] ])
-        expect(sortKeys(map.entries())).toStrictEqual([ [u32(0), u32(4)], [u32(2), u32(8)], [u32(3), u32(7)] ])
-        expect([...map.keys()].sort()).toStrictEqual([ u32(0), u32(2), u32(3) ])
-        expect([...map.values()].sort()).toStrictEqual([ u32(4), u32(7), u32(8) ])
+        expect(sortKeys(map)).toStrictEqual([ [0, 4], [2, 8], [3, 7] ])
+        expect(sortKeys(map.entries())).toStrictEqual([ [0, 4], [2, 8], [3, 7] ])
+        expect([...map.keys()].sort()).toStrictEqual([ 0, 2, 3 ])
+        expect([...map.values()].sort()).toStrictEqual([ 4, 7, 8 ])
 
-        //expect(sortKeys(map.consumeEntries())).toStrictEqual([ [u32(0), u32(4)], [u32(2), u32(8)], [u32(3), u32(7)] ])
+        //expect(sortKeys(map.consumeEntries())).toStrictEqual([ [0, 4], [2, 8], [3, 7] ])
         map.clear()
         expect(sortKeys(map)).toStrictEqual([])
 
