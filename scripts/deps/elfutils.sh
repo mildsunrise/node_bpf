7790a6c2ad2ccb27212974cdc80704b974ce2878
Alba Mendez <me@alba.sh>
2021-08-16 18:41:55 +0200

    ⚙️ scripts to update dependencies
    also remind me of post-upgrade steps,
    because my memory is getting worse

---
+++ scripts/deps/elfutils.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -eEuo pipefail
+cd "$(dirname "$0")/../.."
+
+VERSION="$1"
+
+if [ ! -z "$(git status --porcelain)" ]; then
+  echo "error: working directory must be clean"; exit 1
+fi
+
+cd deps
+
+rm -rf elfutils; mkdir elfutils
+
+URL="https://sourceware.org/elfutils/ftp/$VERSION/elfutils-$VERSION.tar.bz2"
+ORIG="elfutils-$VERSION"
+curl -fL "$URL" | tar -xj "$ORIG"
+( cd "$ORIG";
+  # code (only libelf and common lib)
+  mv lib libelf version.h ../elfutils/
+  # license
+  mv COPYING* ../elfutils/
+  # config template
+  mv config.h.in ../elfutils/
+)
+rm -rf "$ORIG"
+
+if [ -z "$(git status --porcelain)" ]; then
+  echo "error: no changes"; exit 1
+fi
+
+git add -A; git commit -m "deps: upgrade elfutils $VERSION"
+echo -e '\ndone. do the following manual steps and amend the commit:'
+echo ' - update versions["libelf"] in src/binding.cc'
+echo ' - review `git show -- deps/elfutils/config.in.h` and update elfutils_config/config.h'
+echo ' - review buildsystem changes (new files, deps)'
---
+++ scripts/deps/libbpf.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -eEuo pipefail
+cd "$(dirname "$0")/../.."
+
+VERSION="$1"
+
+if [ ! -z "$(git status --porcelain)" ]; then
+  echo "error: working directory must be clean"; exit 1
+fi
+
+cd deps
+
+rm -rf libbpf
+
+URL="https://github.com/libbpf/libbpf/archive/refs/tags/v$VERSION.tar.gz"
+ORIG="libbpf-$VERSION"
+curl -fL "$URL" | tar -xz "$ORIG"
+mv "$ORIG" libbpf
+
+if [ -z "$(git status --porcelain)" ]; then
+  echo "error: no changes"; exit 1
+fi
+
+git add -A; git commit -m "deps: upgrade libbpf $VERSION"
+echo -e '\ndone. do the following manual steps and amend the commit:'
+echo ' - update versions["libbpf"] in src/binding.cc'
+echo ' - review `git show -- deps/libbpf/include/uapi/linux/bpf.h` and update lib/constants.ts'
+echo ' - review `git show -- deps/libbpf/src/libbpf.h` and update lib/exception.ts'
+echo ' - review buildsystem changes'
